475f9a9ce2a58c672ef3aa17a68529afd8050d89
Pjiwm <Pim.Munne@gmail.com>
2021-11-06 19:59:38 +0100

    testing failed tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-sandbox'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-sandbox');
+    expect(app.title).toEqual('angular-sandbox fail');
   });
 
   it('should render title', () => {
