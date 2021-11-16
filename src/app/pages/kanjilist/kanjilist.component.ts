6ead3662310ae5bd33a2a56ff56f5b9dafa46f62
Pjiwm <Pim.Munne@gmail.com>
2021-11-16 17:36:13 +0100

    only show first 5 characters on table

--- src/app/pages/kanjilist/kanjilist.component.ts
+++ src/app/pages/kanjilist/kanjilist.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { KANJILISTS } from 'src/app/models/mock.kanji.list';
+import { Component, OnInit } from '@angular/core'
+import { KANJILISTS } from 'src/app/models/mock.kanji.list'
 
 @Component({
   selector: 'app-kanjilist',
@@ -12,6 +12,14 @@ export class KanjilistComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // this.kanjiLists.push({
+    //   id: 1000,
+    //   name: 'ADDED LIST',
+    //   description: 'XXXX',
+    //   kanji: ['X', 'X', 'X'],
+    //   tags: ['X', 'X'],
+    //   creationDate: new Date("1800-01-22")
+    // })
   }
 
 }
