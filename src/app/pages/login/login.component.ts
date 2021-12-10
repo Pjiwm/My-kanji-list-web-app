6c3c6387fff2001e2a6d69526277dcf142ddda37
Pjiwm <Pim.Munne@gmail.com>
2021-12-10 15:23:56 +0100

    fixed double submit register

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core'
-import { Router } from '@angular/router'
 import { AuthService } from 'src/app/services/auth.service'
 import { ErrorService } from 'src/app/services/error.service'
 
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 import { AuthService } from 'src/app/services/auth.service'
-import { User } from 'src/app/models/user';
 import { ErrorService } from 'src/app/services/error.service'
 
 @Component({
@@ -12,23 +11,20 @@ export class RegisterComponent implements OnInit {
   newUserName: string
   newEmail: string
   newPassword: string
-  user: User
   successfulLogin: Boolean
-  constructor(private authService: AuthService, private errorService: ErrorService) { 
+  constructor(private authService: AuthService, private errorService: ErrorService) {
     this.newUserName = ""
     this.newEmail = ""
     this.newPassword = ""
-    this.user = {userName: this.newUserName, email: this.newEmail, password: this.newPassword, id: 0}
     this.successfulLogin = true
   }
 
   ngOnInit(): void {
     this.errorService.setDefault()
   }
-  
-  onSubmit(): void {
-    this.user = {userName: this.newUserName, email: this.newEmail, password: this.newPassword, id: 0}
-    this.authService.register(this.user)
+
+  async onSubmit(): Promise<void> {
+    this.authService.register(this.newEmail, this.newPassword, this.newUserName)
     this.successfulLogin = false
   }
 
